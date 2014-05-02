7ca4be85623d2694b84fad96d68e1bafb09f9e02
Logan Franken <loganfranken@gmail.com>
2014-05-02 16:48:22 -0700

    Added Housing Option enumeration

---
+++ src/Ucsb/Sa/FinAid/AidEstimation/HousingOption.php
@@ -0,0 +1,11 @@
+<?php
+namespace Ucsb\Sa\FinAid\AidEstimation;
+
+abstract class HousingOption
+{
+    const None = 0;
+    const OnCampus = 1;
+    const OffCampus = 2;
+    const Commuter = 3;
+}
+?>
